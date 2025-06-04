60af36efc86e4e1316fbfc9f759af3ddc1a6535f
snow-apple <shwetlanajha@gmail.com>
2025-06-04 11:38:27 -0400

    parameters for testing mle script

--- MleDecoder.sh
+++ MleDecoder.sh
@@ -4,19 +4,18 @@
 #SBATCH --time=23:55:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=shwetlana.jha@yale.edu
-#SBATCH --array=1-4
+#SBATCH --array=1-2
 #SBATCH --mem-per-cpu=4GB
 #SBATCH --job-name=hy_tcnot
 
 # Define parameter ranges
 ds=3
-de=10
+de=6
 dd=2
-ps=0.01
-pe=0.2
-dp=0.0038
-bp=0
-ns=500
+ps=0.06
+pe=0.13
+dp=0.01
+ns=10
 
 # Set the Slurm job array ID as a variable
 JOB_ARRAY_ID=$SLURM_ARRAY_TASK_ID
@@ -25,4 +24,4 @@ JOB_ARRAY_ID=$SLURM_ARRAY_TASK_ID
 module load miniconda
 conda activate bs
 #Run the python file with the current parameter values
-python mle.py "$ds" "$de" "$dd" "$ps" "$pe" "$dp" "$ns" "$JOB_ARRAY_ID"
+python mle.py "$ds" "$de" "$dd" "$ps" "$pe" "$dp" "$ns" "threshold:$JOB_ARRAY_ID"
