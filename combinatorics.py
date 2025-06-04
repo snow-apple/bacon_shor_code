b268babcc0f96572dea8fb2de6c71d428f097e78
Shwetlana Jha <sjha40@stuy.edu>
2025-06-04 11:07:04 -0400

    created script to run combinatorics

---
+++ combinatorics.py
@@ -0,0 +1,19 @@
+import baconshor
+import sys
+import math
+
+n = len(sys.argv)
+if n != 3:# d and weight
+    print("Not enough arguments")
+    exit()
+d = sys.argv[1]
+w = sys.argv[2]
+
+#figure out how many break up ints of 6e9
+breaks = math.ceil(math.comb(d**2, w)/(6e9))
+sum = 0
+for i in range(breaks):
+    sum += baconshor.list_of_grids(d,w,i)
+
+print(sum)
+
